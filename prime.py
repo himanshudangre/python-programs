d8e6099c677a2122441c79f467e4beb1f1737370
himanshudangre <51230806+himanshudangre@users.noreply.github.com>
2019-10-17 14:21:57 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,17 @@
+def prime(n):
+    x = 1
+    for i in range(2, n):
+        if n % i == 0:
+            x=0
+            break
+        else:
+            x=1
+    return x
+
+num = int(input('enter a number :'))
+result = prime(num)
+
+if result == 1:
+    print(num,'is a prime')
+else:
+    print((num,'is not a prime'))
